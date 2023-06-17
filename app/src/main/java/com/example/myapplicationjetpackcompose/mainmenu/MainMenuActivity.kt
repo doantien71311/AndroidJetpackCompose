423d298d0d52ad2a32bb2e5392c2f62501c1ed14
bui doan tien <mrtien71311@gmail.com>
2023-06-17 15:00:05 +0700

    Tiến commit

--- app/src/main/java/com/example/myapplicationjetpackcompose/mainmenu/MainMenuActivity.kt
+++ app/src/main/java/com/example/myapplicationjetpackcompose/mainmenu/MainMenuActivity.kt
@@ -10,34 +10,67 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavDeepLink
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navDeepLink
-import com.example.myapplicationjetpackcompose.EnumChannel
+import androidx.navigation.navOptions
 import com.example.myapplicationjetpackcompose.EnumDeepLink
 import com.example.myapplicationjetpackcompose.HomeScreen
-import com.example.myapplicationjetpackcompose.LoginViewModel
+import com.example.myapplicationjetpackcompose.NavigationAppHost
 import com.example.myapplicationjetpackcompose.dangnhap.DangNhapPage
 import com.example.myapplicationjetpackcompose.hanhchinhnhansu.nhanvien.NhanVienScreen
 import com.example.myapplicationjetpackcompose.services.EnumFirebaseMessagingService
@@ -50,24 +83,20 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 
 
-
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+
 
 
 @AndroidEntryPoint
 class MainMenuActivity : ComponentActivity() {
 
 
-
     lateinit var navHostController: NavHostController
-    private val viewModel by viewModels<MainMenuStateViewModel>()
 
-    @SuppressLint("CoroutineCreationDuringComposition")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("CoroutineCreationDuringComposition", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -78,11 +107,52 @@ class MainMenuActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val coroutineScope = rememberCoroutineScope()
+
                     navHostController = rememberNavController()
                     NavigationAppHost(navController = navHostController)
 
-                   //CarMenuSrceen(navHostController, current)
 //
+//                    ModalNavigationDrawer(
+//                        drawerContent = { DrawerContent(navHostController, drawerState) },
+//                        drawerState = drawerState
+//                    ) {
+
+//                        Scaffold(topBar = {
+//                            TopAppBar(
+//                                title = { Text(text = "Navigation Drawer") },
+//                                navigationIcon = {
+//
+//                                    IconButton(onClick = {
+//
+//                                        if (drawerState.isClosed) {
+//                                            coroutineScope.launch {
+//                                                drawerState.open()
+//                                            }
+//                                        } else {
+//                                            coroutineScope.launch {
+//                                                drawerState.close()
+//                                            }
+//                                        }
+//
+//                                    }) {
+//                                        Icon(Icons.Filled.Menu, contentDescription = "Drawer Menu.")
+//                                    }
+//
+//                                })
+//                        }) {
+//
+//                            Box (modifier = Modifier.padding(it)) {
+//
+//                                NavigationAppHost(navController = navHostController)
+//                            }
+//                        }
+
+
+                    //}
+
+
                     // Get token
                     // [START log_reg_token]
                     Firebase.messaging.getToken().addOnCompleteListener(OnCompleteListener { task ->
@@ -93,7 +163,7 @@ class MainMenuActivity : ComponentActivity() {
 
                         // Get new FCM registraddtion token
                         val token = task.result
-                        Log.w("TAG", "ToKenPCM: " +token)
+                        Log.w("TAG", "ToKenPCM: " + token)
 
                     })
 
@@ -113,21 +183,25 @@ class MainMenuActivity : ComponentActivity() {
                         navHostController.navigate(m_ma_chucnang)
                     }
 
+
                 }
             }
         }
+
     }
 
-
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun NavigationAppHost(navController: NavHostController) {
 
 
         val context = LocalContext.current
 
-        NavHost(navController = navController,
-                    startDestination =  MainMenuDestination.MAINMENU.route
-           // startDestination =   MainMenuDestination.LOGIN.route
+
+        NavHost(
+            navController = navController,
+            startDestination = MainMenuDestination.MAINMENU.route
+
         ) {
 
             composable(route = MainMenuDestination.LOGIN.route)
@@ -148,13 +222,14 @@ class MainMenuActivity : ComponentActivity() {
 
 
             composable(route = MainMenuDestination.NHAPLIEU_NhanSu_ThongTinPhongVan_Duyet.route,
-                    deepLinks = ( listOf(navDeepLink {
-                uriPattern = EnumDeepLink.MyAppDeepLink + MainMenuDestination.NHAPLIEU_NhanSu_ThongTinPhongVan_Duyet.route
-                action = Intent.ACTION_VIEW
-            }
-            )))
+                deepLinks = (listOf(navDeepLink {
+                    uriPattern =
+                        EnumDeepLink.MyAppDeepLink + MainMenuDestination.NHAPLIEU_NhanSu_ThongTinPhongVan_Duyet.route
+                    action = Intent.ACTION_VIEW
+                }
+                )))
             {
-                ThongTinPhongVanScreen(navController,context)
+                ThongTinPhongVanScreen(navController, context)
             }
 
 
@@ -168,24 +243,30 @@ class MainMenuActivity : ComponentActivity() {
                 route = "nested_graph_route"
             )
             {
-            composable(
-                route = MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route,
-                deepLinks = ( listOf(navDeepLink {
-                   // uriPattern = "myapp://details/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
-                   // uriPattern = "deeplink://details/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
-                   // uriPattern = " https://myapplicationjetpackcompose.page.link/app/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
-                    uriPattern = EnumDeepLink.MyAppDeepLink
-                    action = Intent.ACTION_VIEW
+                composable(
+                    route = MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route,
+                    deepLinks = (listOf(navDeepLink {
+                        // uriPattern = "myapp://details/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
+                        // uriPattern = "deeplink://details/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
+                        // uriPattern = " https://myapplicationjetpackcompose.page.link/app/"+MainMenuDestination.NHAPLIEU_NhanSu_DonDangKyThanhVien_Duyet.route
+                        uriPattern = EnumDeepLink.MyAppDeepLink
+                        action = Intent.ACTION_VIEW
+                    }
+                    ))
+                )
+                {
+                    ThongTinUngVienScreen(navController, context)
                 }
-                ))
-            )
-            {
-                ThongTinUngVienScreen(navController, context)
             }
-           }
 
         }
     }
 
-
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/example/myapplicationjetpackcompose/mainmenu/MainMenuSrceen.kt
+++ app/src/main/java/com/example/myapplicationjetpackcompose/mainmenu/MainMenuSrceen.kt
@@ -4,51 +4,228 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Settings
+
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.myapplicationjetpackcompose.Destination
-import com.example.myapplicationjetpackcompose.model.dto_menu_app
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.example.myapplicationjetpackcompose.R
 import com.example.myapplicationjetpackcompose.model.dto_menu_app_chitiet
-import com.example.myapplicationjetpackcompose.services.EnumFirebaseMessagingService
+
 import com.example.myapplicationjetpackcompose.ui.theme.MyApplicationJetpackComposeTheme
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
+
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalUnitApi::class)
+@Composable
+fun DrawerHeader() {
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "profile pic",
+            modifier = Modifier
+                .clip(CircleShape)
+                .width(150.dp)
+                .border(width = 2.dp, color = Color.Gray, shape = CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "Prashant Developer", fontSize = TextUnit(18F, TextUnitType.Sp), color = Color.Black,)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = "xyz@gmail.com", fontSize = TextUnit(14F, TextUnitType.Sp), color = Color.Gray)
+
+
+
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DrawerContent(navController: NavHostController, drawerState: DrawerState) {
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+            .background(Color.Yellow),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        DrawerHeader()
+        Spacer(modifier = Modifier.height(10.dp))
+        Divider(color = Color.LightGray)
+        Spacer(modifier = Modifier.height(10.dp))
+        NavigationDrawerItems(navController,drawerState)
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        Text(text = "Android Blog Code", fontSize = 16.sp)
+
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationDrawerItems(navController: NavHostController, drawerState: DrawerState) {
+
+    var scope = rememberCoroutineScope()
+
+    var currentBackStackEntryAsState = navController.currentBackStackEntryAsState()
+
+    var destination = currentBackStackEntryAsState.value?.destination
+
+
+
+    NavigationDrawerItem(
+        icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
+        label = { Text(text = "Home") },
+        selected = destination?.route == "HomePage",
+        onClick = {
+            navController.navigate("HomePage", navOptions {
+                this.launchSingleTop = true
+                this.restoreState = true
+
+            })
+            scope.launch {
+                drawerState.close()
+            }
+
+        }, modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+    )
+
+    Spacer(modifier = Modifier.height(10.dp))
+    NavigationDrawerItem(
+        icon = { Icon(Icons.Filled.Place, "About") },
+        label = { Text(text = "About") },
+        selected = destination?.route == "AboutPage",
+
+        onClick = {
+            navController.navigate("AboutPage", navOptions {
+                this.launchSingleTop = true
+                this.restoreState = true
+
+            })
+            scope.launch {
+                drawerState.close()
+            }
+        },
+        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+
+
+    NavigationDrawerItem(
+        icon = { Icon(Icons.Filled.Settings, "Setting") },
+
+        label = { Text(text = "Setting") },
+        selected = destination?.route == "SettingPage",
+        onClick = {
+            navController.navigate("SettingPage", navOptions {
+                this.launchSingleTop = true
+                this.restoreState = true
+            })
+            scope.launch {
+                drawerState.close()
+            }
+        },
+        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+    )
+
+    Spacer(modifier = Modifier.height(10.dp))
+
+
+    NavigationDrawerItem(
+        icon = { Icon(Icons.Filled.Settings, "Đăng xuất") },
+
+        label = { Text(text = "Đăng xuất") },
+        selected = false,
+        onClick = {
+            GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
+
+                // mainMenuViewModel.clearDataStore()
+                navController.navigate(MainMenuDestination.LOGIN.route)
+                scope.launch {
+                    drawerState.close()
+                }
+            }
+        },
+        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+    )
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun MainMenuSrceen (
@@ -70,6 +247,7 @@ fun MainMenuSrceen (
 
 
 
+
 //    var isLoading by remember {
 //        mutableStateOf(true)
 //    }
@@ -78,87 +256,106 @@ fun MainMenuSrceen (
 //        delay(5000)
 //        isLoading = false
 //    }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-        MainMenuLoadingAnimation(
+        MainMenuLoadingAnimation (
 
             isLoadding = mainMenuViewModel.isLoadding,
             //isLoadding = isLoading,
             contentAfterLoading = {
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            ModalDrawerSheet {
+                                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                                Divider()
+                                NavigationDrawerItem(
+                                    label = { Text(text = "Drawer Item") },
+                                    selected = false,
+                                    onClick = { /*TODO*/ }
+                                )
+                                // ...other drawer items
 
-                LazyColumn(
-                    //  modifier = Modifier.verticalScroll(rememberScrollState())
-                )
+                            }
+                        }
+                    },
+                ) {
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = { Text(text = "Main menu") },
+                        navigationIcon = {
 
-                {
+                            IconButton(onClick = {
 
-                    itemsIndexed(
-                        items = mainMenuViewModel.ListMenuApp.toTypedArray()
+                                scope.launch {
+                                    drawerState.apply {
+                                        if (isClosed) open() else close()
+                                    }
+                                }
+                            }) {
+                                Icon(Icons.Filled.Menu, contentDescription = null)
+                            }
 
-                    ) { index, item ->
-
-                        Text(
-                            text = item.ten_chucnang ?: "",
-                            //  modifier = Modifier.align(Alignment.CenterHorizontally)
-
-
-                        )
-
-                        Button(onClick = {
-
-
-
-                        GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
-
-                            mainMenuViewModel.clearDataStore()
-                            navController.navigate(MainMenuDestination.LOGIN.route)
                         }
 
-                        }) {
+                    )
+                }) {
 
-                            Text(text = "Đăng xuất")
+                    Box (modifier = Modifier.padding(it)) {
 
-                        }
-
-                        RowsCarMenuSrceen(
-                            navController,
-                            context,
-                            item.menu_app_chitiet!!.toTypedArray()
+                        LazyColumn(
+                            //  modifier = Modifier.verticalScroll(rememberScrollState())
                         )
+
+                        {
+
+                            itemsIndexed(
+                                items = mainMenuViewModel.ListMenuApp.toTypedArray()
+
+                            ) { index, item ->
+
+                                Text(
+                                    text = item.ten_chucnang ?: "",
+                                    //  modifier = Modifier.align(Alignment.CenterHorizontally)
+
+
+                                )
+
+                                Button(onClick = {
+
+
+
+                                    GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
+
+                                        mainMenuViewModel.clearDataStore()
+                                        navController.navigate(MainMenuDestination.LOGIN.route)
+                                    }
+
+                                }) {
+
+                                    Text(text = "Đăng xuất")
+
+                                }
+
+                                RowsCarMenuSrceen(
+                                    navController,
+                                    context,
+                                    item.menu_app_chitiet!!.toTypedArray()
+                                )
+
+                            }
+                        }
 
                     }
                 }
 
 
+
+            }
             }
         )
-
-
-//    LazyColumn (
-//        //  modifier = Modifier.verticalScroll(rememberScrollState())
-//    )
-//
-//    {
-//
-//            itemsIndexed(
-//                items = mainMenuViewModel.ListMenuApp.toTypedArray()
-//
-//            ) { index, item ->
-//
-//                Text(
-//                    text = item.ten_chucnang!!,
-//                    //  modifier = Modifier.align(Alignment.CenterHorizontally)
-//
-//
-//                )
-//
-//                RowsCarMenuSrceen(
-//                    navController,
-//                    context,
-//                    item.menu_app_chitiet!!.toTypedArray()
-//                )
-//
-//            }
-//        }
 
 
    }
@@ -193,18 +390,16 @@ fun RowsCarMenuSrceen(
 
 
 
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun CarMenuSrceenPreview() {
-    MyApplicationJetpackComposeTheme {
-
-           var lis : List<dto_menu_app>
-           lis = ArrayList<dto_menu_app>()
 
 
 
-        //CarMenuSrceen(lis.toTypedArray())
-    }
-}
+//@Preview(showBackground = true)
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+//@Composable
+//fun CarMenuSrceenPreview() {
+//    MyApplicationJetpackComposeTheme {
+//
+//      //  Header()
+//    }
+//}
 
