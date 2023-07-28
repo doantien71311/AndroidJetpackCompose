6b1bc14e7ec921d0fecc12ed9a078c95f4e506bd
bui doan tien <mrtien71311@gmail.com>
2023-07-28 15:09:40 +0700

    ddddd dsf sadd

--- app/src/main/java/com/example/myapplicationjetpackcompose/hanhchinhnhansu/nhanvien/NhanVienViewModel.kt
+++ app/src/main/java/com/example/myapplicationjetpackcompose/hanhchinhnhansu/nhanvien/NhanVienViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myapplicationjetpackcompose.hanhchinhnhansu.nhanvien
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -13,12 +14,18 @@ import com.example.myapplicationjetpackcompose.lookup.chucvu.LookupChucVuViewMod
 import com.example.myapplicationjetpackcompose.model.dm_nhanvien_cus
 import com.example.myapplicationjetpackcompose.model.dm_ungvien_cus
 import com.example.myapplicationjetpackcompose.model.dto_menu_app
+import com.example.myapplicationjetpackcompose.model.response_dm_nhanvien_cus
+import com.example.myapplicationjetpackcompose.model.response_dto_menu_app
 import com.example.myapplicationjetpackcompose.services.DataStoreCustomServices
 import com.example.myapplicationjetpackcompose.services.DataStoreServices
 import com.example.myapplicationjetpackcompose.services.IDataStoreServies
+import com.example.myapplicationjetpackcompose.services.RetrofitService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
 //@HiltViewModel
@@ -64,12 +71,39 @@ class NhanVienViewModel @Inject constructor (
 
     init {
 
+        loadData()
 
     }
 
     private fun loadData() {
 
+        viewModelScope.launch {
+            RetrofitService.IRetrofitService
+                .getNhanVienLayDS(dataStoreServies.getBearToken(), dataStoreServies.getUserName()  )
+                .enqueue(object : Callback<response_dm_nhanvien_cus?> {
+                    override fun onResponse(
+                        call: Call<response_dm_nhanvien_cus?>,
+                        response: Response<response_dm_nhanvien_cus?>
+                    ) {
+                        response.body()?.data?.let {
+                            state = it
+                        }
 
+                    }
+
+                    override fun onFailure(
+                        call: Call<response_dm_nhanvien_cus?>,
+                        t: Throwable
+                    ) {
+
+                        var s = t.localizedMessage
+                        Log.e("Faild", t.message.toString())
+                    }
+
+                })
+
+
+        }
     }
 
     private fun saveData() {
