c7391c931b80db3e79d709f1ea6edd427a93d770
bui doan tien <mrtien71311@gmail.com>
2023-08-19 17:03:47 +0700

    commit khao sat b2

--- app/src/main/java/com/example/myapplicationjetpackcompose/services/UrlApi.kt
+++ app/src/main/java/com/example/myapplicationjetpackcompose/services/UrlApi.kt
@@ -9,8 +9,8 @@ object url_api {
 //    const val password : String = "6CC0BCCBEA7D5E8F678F14FC5400E251"
 
 
-    const val httpAdress : String = "https://fwm-api.theworldlink.vn/"
-    const val username : String = "6BF0C02971B2F445"
+    const val httpAdress : String = "https://twl-api.theworldlink.vn/"
+    const val username : String = "B8BAC6B049709B134AB4B54F57D9E1C0DB068E91091B1047"
     const val password : String = "6CC0BCCBEA7D5E8F678F14FC5400E251"
 
     const val authenticate_login: String = "api/Authenticate/login"
